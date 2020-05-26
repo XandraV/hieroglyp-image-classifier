971f61c7481282fe382b4e888612907b0908c3c0
XandraV <veresalexandra95@gmail.com>
2020-05-27 00:30:39 +0100

    react app flask backend

---
+++ app/backend/run.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+from PIL import Image
+from flask_cors import CORS
+from predict import predict_image
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/predict0', methods=["POST"])
+def get_prediction():
+    file = request.files['file']
+    im = Image.open(file).convert('RGB')
+    res = predict_image(im)
+    return {'prediction': str(res)}
+
+if __name__ == "__main__":
+
+    # Load this config object for development mode
+    app.config.from_object('configurations.DevelopmentConfig')
+    app.run()
